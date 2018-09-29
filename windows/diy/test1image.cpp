b10d9998bc81b36adf683d809eda7d14c6223b9f
s <s>
2018-09-29 14:23:50 +0800

    change test1image by project - classification with class - Classifier

--- windows/diy/test1image.cpp
+++ windows/diy/test1image.cpp
@@ -1,85 +1,36 @@
 
-#include <gflags/gflags.h>
-#include <glog/logging.h>
-
-#include <cstring>
-#include <map>
-#include <string>
-#include <vector>
-
-#include "boost/algorithm/string.hpp"
-#include "caffe/caffe.hpp"
-#include "caffe/util/signal_handler.h"
-
-using caffe::Blob;
-using caffe::Caffe;
-using caffe::Net;
-using caffe::Layer;
-using caffe::Solver;
-using caffe::shared_ptr;
-using caffe::string;
-using caffe::Timer;
-using caffe::vector;
-using std::ostringstream;
-
-DEFINE_string(gpu, "",
-    "Optional; run in GPU mode on given device IDs separated by ','."
-    "Use '-gpu all' to run on all available GPUs. The effective training "
-    "batch size is multiplied by the number of devices.");
-DEFINE_string(solver, "",
-    "The solver definition protocol buffer text file.");
-DEFINE_string(model, "",
-    "The model definition protocol buffer text file.");
-DEFINE_string(snapshot, "",
-    "Optional; the snapshot solver state to resume training.");
-DEFINE_string(weights, "",
-    "Optional; the pretrained weights to initialize finetuning, "
-    "separated by ','. Cannot be set simultaneously with snapshot.");
-DEFINE_int32(iterations, 1,
-    "The number of iterations to run.");
-
-// Test: score a model.
-int test() {
-
-    Caffe::SetDevice(0);
-    Caffe::set_mode(Caffe::GPU);
-  // Instantiate the caffe net.
-  Net<float> caffe_net(FLAGS_model, caffe::TEST);
-  caffe_net.CopyTrainedLayersFrom(FLAGS_weights);
-
-  vector<int> test_score_output_id;
-  vector<float> test_score;
-
-    float iter_loss;
-    const vector<Blob<float>*>& result =
-        caffe_net.Forward(&iter_loss);
-
-    int idx = 0;
-    for (int j = 0; j < result.size(); ++j) {
-      const float* result_vec = result[j]->cpu_data();
-      for (int k = 0; k < result[j]->count(); ++k, ++idx) {
-        const float score = result_vec[k];
-          test_score.push_back(score);
-          test_score_output_id.push_back(j);
-        const std::string& output_name = caffe_net.blob_names()[
-            caffe_net.output_blob_indices()[j]];
-        LOG(INFO) << "Batch " << 0 << ", " << output_name << " = " << score;
-      }
-    }
-	LOG(INFO) << "Loss: " << iter_loss;
-
-  return 0;
-}
-
+#include "Classifier.h"
 
 int main(int argc, char** argv) {
-  // Print output to stderr (while still logging).
-  FLAGS_alsologtostderr = 1;
-  // Set version
-  gflags::SetVersionString(AS_STRING(CAFFE_VERSION));
+	if (argc != 6) {
+		std::cerr << "Usage: " << argv[0]
+			<< " deploy.prototxt network.caffemodel"
+			<< " mean.binaryproto labels.txt img.jpg" << std::endl;
+		return 1;
+	}
 
-  // Run tool or show usage.
-  caffe::GlobalInit(&argc, &argv);
-  
-  return	test();
+	::google::InitGoogleLogging(argv[0]);
+
+	string model_file = argv[1];
+	string trained_file = argv[2];
+	string mean_file = argv[3];
+	string label_file = argv[4];
+	Classifier classifier(model_file, trained_file, mean_file, label_file);
+
+	string file = argv[5];
+
+	std::cout << "---------- Prediction for "
+		<< file << " ----------" << std::endl;
+
+	cv::Mat img = cv::imread(file, -1);
+	CHECK(!img.empty()) << "Unable to decode image " << file;
+	std::vector<Prediction> predictions = classifier.Classify(img);
+
+	/* Print the top N predictions. */
+	for (size_t i = 0; i < predictions.size(); ++i) {
+		Prediction p = predictions[i];
+		std::cout << std::fixed << std::setprecision(4) << p.second << " - \""
+			<< p.first << "\"" << std::endl;
+	}
 }
+
