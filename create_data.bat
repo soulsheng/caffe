6038df778368e9666b7495e9711768683b2620b6
s <s>
2019-02-16 17:41:45 +0800

    succeed to create lmdb files for trainval and test, replace py cmd by exe cmd

--- create_data.bat
+++ create_data.bat
@@ -6,11 +6,13 @@ Echo caffe create_annoset Batch
 ::  - G:\file\code\dnn\caffe-ssd-win-diy\data\VOC0712\trainval_lmdb
 ::  - G:\file\code\dnn\caffe-ssd-win-diy\data\VOC0712\test_lmdb
 
+set TOOLS=G:\file\code\dnn\caffe-ssd-win-diy\Build\x64\Release
+
 set root_dir=G:\file\code\dnn\caffe-ssd-win-diy
 cd %root_dir%
 
-set redo=1
-set data_root_dir=data\VOC0712
+
+set data_root_dir=%root_dir%\data\VOCdevkit\
 set mapfile=%data_root_dir%\labelmap_voc.prototxt
 set anno_type=detection
 set db=lmdb
@@ -19,23 +21,21 @@ set max_dim=0
 set width=0
 set height=0
 
-set "extra_cmd=--encode-type=jpg --encoded"
+set "extra_cmd=--encode_type=jpg --encoded=True"
 
-if %redo%==1 (
-	set "extra_cmd=%extra_cmd% --redo"
-)
+
 
 for %%s in (trainval test) do (
 echo Creating %%s lmdb...
 
-python %root_dir%\scripts\create_annoset.py ^
-	--anno-type=%anno_type% ^
-	--label-map-file=%mapfile% ^
-	--min-dim=%min_dim% ^
-	--max-dim=%max_dim% ^
-	--resize-width=%width% ^
-	--resize-height=%height% ^
-	--check-label %extra_cmd% ^
+%TOOLS%\convert_annoset ^
+	--anno_type=%anno_type% ^
+	--label_map_file=%mapfile% ^
+	--min_dim=%min_dim% ^
+	--max_dim=%max_dim% ^
+	--resize_width=%width% ^
+	--resize_height=%height% ^
+	--check_label %extra_cmd% ^
 	%data_root_dir% ^
 	%data_root_dir%\%%s.txt ^
 	%data_root_dir%\%%s_%db%
