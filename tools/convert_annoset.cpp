49260b2a60a9eaa6b4ab14718752819ca81ec45d
s <s>
2019-02-16 10:39:47 +0800

    update python/scripts/tools

--- tools/convert_annoset.cpp
+++ tools/convert_annoset.cpp
@@ -1,20 +1,27 @@
-// This program converts a set of images to a lmdb/leveldb by storing them
-// as Datum proto buffers.
+// This program converts a set of images and annotations to a lmdb/leveldb by
+// storing them as AnnotatedDatum proto buffers.
 // Usage:
-//   convert_imageset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME
+//   convert_annoset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME
 //
-// where ROOTFOLDER is the root folder that holds all the images, and LISTFILE
-// should be a list of files as well as their labels, in the format as
-//   subfolder1/file1.JPEG 7
+// where ROOTFOLDER is the root folder that holds all the images and
+// annotations, and LISTFILE should be a list of files as well as their labels
+// or label files.
+// For classification task, the file should be in the format as
+//   imgfolder1/img1.JPEG 7
+//   ....
+// For detection task, the file should be in the format as
+//   imgfolder1/img1.JPEG annofolder1/anno1.xml
 //   ....
 
 #include <algorithm>
 #include <fstream>  // NOLINT(readability/streams)
+#include <map>
 #include <string>
 #include <utility>
 #include <vector>
 
 #include "boost/scoped_ptr.hpp"
+#include "boost/variant.hpp"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
@@ -33,7 +40,19 @@ DEFINE_bool(gray, false,
 DEFINE_bool(shuffle, false,
     "Randomly shuffle the order of images and their labels");
 DEFINE_string(backend, "lmdb",
-        "The backend {lmdb, leveldb} for storing the result");
+    "The backend {lmdb, leveldb} for storing the result");
+DEFINE_string(anno_type, "classification",
+    "The type of annotation {classification, detection}.");
+DEFINE_string(label_type, "xml",
+    "The type of annotation file format.");
+DEFINE_string(label_map_file, "",
+    "A file with LabelMap protobuf message.");
+DEFINE_bool(check_label, false,
+    "When this option is on, check that there is no duplicated name/label.");
+DEFINE_int32(min_dim, 0,
+    "Minimum dimension images are resized to (keep same aspect ratio)");
+DEFINE_int32(max_dim, 0,
+    "Maximum dimension images are resized to (keep same aspect ratio)");
 DEFINE_int32(resize_width, 0, "Width images are resized to");
 DEFINE_int32(resize_height, 0, "Height images are resized to");
 DEFINE_bool(check_size, false,
@@ -53,16 +72,14 @@ int main(int argc, char** argv) {
   namespace gflags = google;
 #endif
 
-  gflags::SetUsageMessage("Convert a set of images to the leveldb/lmdb\n"
-        "format used as input for Caffe.\n"
+  gflags::SetUsageMessage("Convert a set of images and annotations to the "
+        "leveldb/lmdb format used as input for Caffe.\n"
         "Usage:\n"
-        "    convert_imageset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME\n"
-        "The ImageNet dataset for the training demo is at\n"
-        "    http://www.image-net.org/download-images\n");
+        "    convert_annoset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME\n");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   if (argc < 4) {
-    gflags::ShowUsageWithFlagsRestrict(argv[0], "tools/convert_imageset");
+    gflags::ShowUsageWithFlagsRestrict(argv[0], "tools/convert_annoset");
     return 1;
   }
 
@@ -70,16 +87,32 @@ int main(int argc, char** argv) {
   const bool check_size = FLAGS_check_size;
   const bool encoded = FLAGS_encoded;
   const string encode_type = FLAGS_encode_type;
+  const string anno_type = FLAGS_anno_type;
+  AnnotatedDatum_AnnotationType type;
+  const string label_type = FLAGS_label_type;
+  const string label_map_file = FLAGS_label_map_file;
+  const bool check_label = FLAGS_check_label;
+  std::map<std::string, int> name_to_label;
 
   std::ifstream infile(argv[2]);
-  std::vector<std::pair<std::string, int> > lines;
-  std::string line;
-  size_t pos;
+  std::vector<std::pair<std::string, boost::variant<int, std::string> > > lines;
+  std::string filename;
   int label;
-  while (std::getline(infile, line)) {
-    pos = line.find_last_of(' ');
-    label = atoi(line.substr(pos + 1).c_str());
-    lines.push_back(std::make_pair(line.substr(0, pos), label));
+  std::string labelname;
+  if (anno_type == "classification") {
+    while (infile >> filename >> label) {
+      lines.push_back(std::make_pair(filename, label));
+    }
+  } else if (anno_type == "detection") {
+    type = AnnotatedDatum_AnnotationType_BBOX;
+    LabelMap label_map;
+    CHECK(ReadProtoFromTextFile(label_map_file, &label_map))
+        << "Failed to read label map file.";
+    CHECK(MapNameToLabel(label_map, check_label, &name_to_label))
+        << "Failed to convert name to label.";
+    while (infile >> filename >> labelname) {
+      lines.push_back(std::make_pair(filename, labelname));
+    }
   }
   if (FLAGS_shuffle) {
     // randomly shuffle data
@@ -91,6 +124,8 @@ int main(int argc, char** argv) {
   if (encode_type.size() && !encoded)
     LOG(INFO) << "encode_type specified, assuming encoded=true.";
 
+  int min_dim = std::max<int>(0, FLAGS_min_dim);
+  int max_dim = std::max<int>(0, FLAGS_max_dim);
   int resize_height = std::max<int>(0, FLAGS_resize_height);
   int resize_width = std::max<int>(0, FLAGS_resize_width);
 
@@ -101,13 +136,14 @@ int main(int argc, char** argv) {
 
   // Storing to db
   std::string root_folder(argv[1]);
-  Datum datum;
+  AnnotatedDatum anno_datum;
+  Datum* datum = anno_datum.mutable_datum();
   int count = 0;
   int data_size = 0;
   bool data_size_initialized = false;
 
   for (int line_id = 0; line_id < lines.size(); ++line_id) {
-    bool status;
+    bool status = true;
     std::string enc = encode_type;
     if (encoded && !enc.size()) {
       // Guess the encoding type from the file name
@@ -118,16 +154,28 @@ int main(int argc, char** argv) {
       enc = fn.substr(p);
       std::transform(enc.begin(), enc.end(), enc.begin(), ::tolower);
     }
-    status = ReadImageToDatum(root_folder + lines[line_id].first,
-        lines[line_id].second, resize_height, resize_width, is_color,
-        enc, &datum);
-    if (status == false) continue;
+    filename = root_folder + lines[line_id].first;
+    if (anno_type == "classification") {
+      label = boost::get<int>(lines[line_id].second);
+      status = ReadImageToDatum(filename, label, resize_height, resize_width,
+          min_dim, max_dim, is_color, enc, datum);
+    } else if (anno_type == "detection") {
+      labelname = root_folder + boost::get<std::string>(lines[line_id].second);
+      status = ReadRichImageToAnnotatedDatum(filename, labelname, resize_height,
+          resize_width, min_dim, max_dim, is_color, enc, type, label_type,
+          name_to_label, &anno_datum);
+      anno_datum.set_type(AnnotatedDatum_AnnotationType_BBOX);
+    }
+    if (status == false) {
+      LOG(WARNING) << "Failed to read " << lines[line_id].first;
+      continue;
+    }
     if (check_size) {
       if (!data_size_initialized) {
-        data_size = datum.channels() * datum.height() * datum.width();
+        data_size = datum->channels() * datum->height() * datum->width();
         data_size_initialized = true;
       } else {
-        const std::string& data = datum.data();
+        const std::string& data = datum->data();
         CHECK_EQ(data.size(), data_size) << "Incorrect data field size "
             << data.size();
       }
@@ -137,7 +185,7 @@ int main(int argc, char** argv) {
 
     // Put in db
     string out;
-    CHECK(datum.SerializeToString(&out));
+    CHECK(anno_datum.SerializeToString(&out));
     txn->Put(key_str, out);
 
     if (++count % 1000 == 0) {
